ce33efde5c0b9c2b7629ca57d50731f5b7c1085c
Shamera Boozer <snboozer@gmail.com>
2019-07-28 10:25:55 -0400

    updated code

--- liriNode.js
+++ liriNode.js
@@ -13,15 +13,23 @@ var command = process.argv[2]
 var target = process.argv[3]
 
 if (command === "spotify-this-song"){
+
 }
 else if (command=== "concert-this"){
     axios.get("https://rest.bandsintown.com/artists/" + target + "/events?app_id=codingbootcamp").then(function(response){
-        console.log(response)
+        console.log("-----------------------------------------------------------------------")
+        // to get name
+    console.log(response.data[0].venue.name)
+    console.log(response.data[0].venue.city)
+    
+    // console.log(response.data[0].datetime)
+    // moment(response.data[0].datetime).formet('DD-MM-YYYY')
+    console.log(moment(response.data[0].datetime).format('MM-DD-YYYY'))
     })
 
 }
 else if (command=== "movie-this"){
-
+axios.get()
 }
 else if (command==="do-what-it-says"){
 
