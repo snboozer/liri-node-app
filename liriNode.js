05869dea947dc4143c649f9f0e7ed0309f77ec96
Shamera Boozer <snboozer@gmail.com>
2019-07-28 12:19:50 -0400

    updated code and readMe

--- liriNode.js
+++ liriNode.js
@@ -12,26 +12,35 @@ var spotify = new Spotify(keys.spotify);
 var command = process.argv[2]
 var target = process.argv[3]
 
-if (command === "spotify-this-song"){
+if (command === "spotify-this-song") {
 
 }
-else if (command=== "concert-this"){
-    axios.get("https://rest.bandsintown.com/artists/" + target + "/events?app_id=codingbootcamp").then(function(response){
+else if (command === "concert-this") {
+    axios.get("https://rest.bandsintown.com/artists/" + target + "/events?app_id=codingbootcamp").then(function (response) {
         console.log("-----------------------------------------------------------------------")
         // to get name
-    console.log(response.data[0].venue.name)
-    console.log(response.data[0].venue.city)
-    
-    // console.log(response.data[0].datetime)
-    // moment(response.data[0].datetime).formet('DD-MM-YYYY')
-    console.log(moment(response.data[0].datetime).format('MM-DD-YYYY'))
+        console.log(response.data[0].venue.name)
+        console.log(response.data[0].venue.city)
+
+        // console.log(response.data[0].datetime)
+        // moment(response.data[0].datetime).formet('DD-MM-YYYY')
+        console.log(moment(response.data[0].datetime).format('MM-DD-YYYY'))
     })
 
 }
-else if (command=== "movie-this"){
-axios.get()
+else if (command === "movie-this") {
+    axios.get("http://www.omdbapi.com/?t=" + target + "&y=&plot=short&apikey=trilogy").then(function (response) {
+        console.log("-----------------------------------------------------------------------")
+    console.log(response.data.Title)
+    console.log(response.data.Year)
+    console.log(response.data.Rated)
+    console.log(response.data.Country)
+    console.log(response.data.Language)
+    console.log(response.data.Plot)
+    console.log(response.data.Actors)
+    })
 }
-else if (command==="do-what-it-says"){
+else if (command === "do-what-it-says") {
 
 }
-else {console.log("Please choose one")}
+else { console.log("Please choose one") }
