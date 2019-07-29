aefc4909ed447bd5d073f1379b49d1e6a1cd9cac
Shamera Boozer <snboozer@gmail.com>
2019-07-29 10:50:24 -0400

    updated code

--- liriNode.js
+++ liriNode.js
@@ -7,21 +7,30 @@ var Spotify = require("node-spotify-api")
 var fs = require("fs")
 
 
-
+var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2]
-var target = process.argv[3]
+var target = process.argv.slice(3).join("")
 
-if (command === "spotify-this-song"){
-    function getSpotify(songName){
-        var spotify = new Spotify(keys.spotify);
+if (command === "spotify-this-song") {
+    spotify.search({ type: 'track', query: target, limit: 1 }).then(function (response) {
+        var spotifyArr = response.tracks.items;
+        for (i = 0; i < spotifyArr.length; i++) {
+            console.log(response);
+            console.log(response.tracks.items[i].album.artists[0].name)
+            console.log(response.tracks.items[i].name)
+            console.log(response.tracks.items[i].external_urls.spotify)
+            // function spotify(){
+            //     if (!target) {target = "the sign"};
 
-    spotify.search({type:'track', query:songName})
-    console.log (data)
+        }
+    })
 }
 
 
-}
+
+
+
 else if (command === "concert-this") {
     axios.get("https://rest.bandsintown.com/artists/" + target + "/events?app_id=codingbootcamp").then(function (response) {
         console.log("-----------------------------------------------------------------------")
@@ -48,16 +57,16 @@ else if (command === "movie-this") {
     })
 }
 else if (command === "do-what-it-says") {
-    fs.readFile('random.txt', 'utf8',function(error, data) {
+    fs.readFile('random.txt', 'utf8', function (error, data) {
 
         if (error) {
-          return console.log(error);
+            return console.log(error);
         }
-      
+
         console.log(data);
-      
-      });
-      
+
+    });
+
 
 }
 else { console.log("Please choose one") }
